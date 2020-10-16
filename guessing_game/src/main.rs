dd3a1637377d3913dd7157d66c0201050eab8cd2
Saeid Alidadi <alidadisaeid@gmail.com>
2020-10-16 22:12:12 +0330

    less_1: exit when user win

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,23 +1,30 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("Please input your guess.");
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line.");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    println!("You guess: {}", guess);
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        println!("You guess: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
