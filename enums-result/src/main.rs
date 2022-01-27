b38f451a3c2b3d7d3c47b65701f0f8821dedde83
Saeid Alidadi <alidadisaeid@gmail.com>
2022-01-27 20:56:29 +0330

    enums: result genric enum for error handling

---
+++ enums-result/src/main.rs
@@ -0,0 +1,27 @@
+/**
+ * Reuslt is a generic enum that is so common for rust's error handling
+ * ```
+ * enum Resukt<T, E> {
+ *   Ok(T),
+ *   Err(E)
+ * }
+ * ```
+ * It is the idiomatic way in which the language does error handling.
+ */
+
+
+ fn maight_fail(n: i16) -> Result<f32, String> {
+    if n < 3 {
+        Ok(3.2)
+    } else {
+        Err(String::from("N should be less than 3"))
+    }
+ }
+fn main() {
+    let result = maight_fail(4);
+
+    match result {
+        Ok(t) => println!("{}", t),
+        Err(e) => println!("Error: {}", e)
+    };
+}
